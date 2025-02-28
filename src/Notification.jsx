3476cf371ec4c0ea8d45b0ce24d9eb17846d4f1e
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-02-28 23:34:49 +0530

    deploy

---
+++ src/Notification.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Not from "./assets/Not.svg";
+const Notification = () => {
+  return (
+    <div className="mt-10">
+    <h1 className="font-bold text-center w-full  text-amber-500 tracking-widest text-2xl mb-10">
+      CHECK YOUR CONNECTION.....!
+    </h1>
+    <img src={Not} alt="" className="w-full h-80" />
+  </div>
+  )
+}
+
+export default Notification
