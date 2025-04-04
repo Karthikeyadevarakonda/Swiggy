e8cd288ee243383869c9655be4b3a9b3e7c451d3
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-04-04 14:03:30 +0530

    deploy

--- src/EmptyCart.jsx
+++ src/EmptyCart.jsx
@@ -1,9 +1,10 @@
-
-import emptyCart from './assets/emptyCart.svg'
+import emptyCart from './assets/emptyCart.avif'
 const EmptyCart = () => {
   return (
-           <div>
-           <img src={emptyCart} alt="" className='w-[80%] h-80 m-auto mt-10'  />
+           <div className='flex flex-col justify-center items-center '>
+           <img src={emptyCart} alt="" className=' h-80'  />
+           <h1 className='font-bold mt-4 text-2xl'>Your cart is empty</h1>
+           <p className='font-semibold text-gray-500 mt-2'>You can go to home page to view more restaurants</p>
            </div>
   )
 }
